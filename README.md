d1c942254b7d99beee9d76c8524ef408ad0850dd
Donghyuk <mrgamza@gmail.com>
2023-05-02 22:05:31 +0900

    Project init

---
+++ README.md
@@ -0,0 +1,2 @@
+# springboot-jpa-lombok
+Springboot, JPA, Lombok를 이용한 프로젝트 개발 예제 입니다.d
---
+++ src/test/java/com/example/jpa/JpaExampleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
+
+
+@SpringBootTest
+class JpaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals("a", "a");
+
+	}
+}
